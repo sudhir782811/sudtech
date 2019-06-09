62ea1a61276fafe4b9f3dac3d5ef7d917433ffd2
root <root@ip-192-168-1-15.us-east-2.compute.internal>
2019-06-09 06:46:20 +0000

    got notification of instance in case it down

---
+++ downalert.py
@@ -0,0 +1,8 @@
+import boto3
+
+client = boto3.client('sns')
+
+def lambda_handler(event, context):
+    topic_arn = 'arn:aws:sns:us-east-2:131081180442:PRODALERT'
+    message = 'Server Down please chek'
+    client.publish(TopicArn=topic_arn,Message=message)
