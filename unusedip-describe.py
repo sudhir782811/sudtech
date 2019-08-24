9e40f3dd497fa9d34b50a3e2ba510a15b84e79a9
sudhir <sudhir782811@gmail.com>
2019-08-24 12:57:35 -0700

    unsued-ip describe

--- unusedip-describe.py
+++ unusedip-describe.py
@@ -1,9 +1,8 @@
-import boto3
+mport boto3
 client = boto3.client('ec2')
 def lambda_handler(event, context):
     ipresp = client.describe_addresses()
     for i in ipresp['Addresses']:
         if 'InstanceId' in i:
             print('{} this is in use'.format(i['PublicIp']))
-        else:
-            print('{} this is not in use'.format(i['PublicIp']))
+        
